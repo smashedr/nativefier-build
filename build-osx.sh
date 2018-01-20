cd58c9e8b4422677947d22f728ca09ad11adec20
Shane <shane@shanerice.me>
2018-01-20 15:36:56 -0800

    Adding to GitLab

---
+++ build-osx.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -e
+
+export PATH="${PATH}:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+
+echo "PATH:          ${PATH}"
+echo "WORKSPACE:     ${WORKSPACE}"
+echo "app_name:      ${app_name}"
+echo "app_url:       ${app_url}"
+echo "favicon_url:   ${favicon_url}"
+echo "appdmg_json    ${appdmg_json}"
+
+favicon=$(basename "$favicon_url")
+wget "$favicon_url"
+
+if [  "${favicon##*.}" != "icns" ] && [  "${favicon##*.}" != "tiff" ];then
+    sips -s format tiff "./${favicon}" --out ./favicon.tiff
+    favicon="favicon.tiff"
+fi
+if [  "${favicon##*.}" = "tiff" ];then
+    tiff2icns -noLarge "./${favicon}" ./favicon.icns
+    favicon="favicon.icns"
+fi
+if [  "${favicon##*.}" != "icns" ];then
+    echo "Error creating favicon.icns"
+    exit 1
+fi
+
+out=$(nativefier --name "${app_name}" --icon "./${favicon}" --width "${window_width}" --height "${window_height}" --user-agent "${app_name} (electron)" "${app_url}")
+dir=$(echo "${out}" | tail -n1 | awk -F' to ' '{print $2}')
+archive="$(echo ${app_name} | sed 's/ /-/g').tar.gz"
+
+cd "${dir}"
+tar -czf "${archive}" "${app_name}.app"
+mv "${archive}" "${WORKSPACE}"
