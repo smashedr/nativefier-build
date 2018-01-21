09ba7b9abb24087f7d9581c7893c4c03b911cae2
Shane <shane@shanerice.me>
2018-01-20 16:59:41 -0800

    Testing

--- build-win.ps1
+++ build-win.ps1
@@ -17,5 +17,5 @@ magick.exe -version
 Invoke-WebRequest -Uri "$Env:favicon_url" -OutFile ".\$favicon"
 
 if ("$ext" -ne "png") {
-    magick.exe convert ".\$favicon" ".\favicon.png"
+    magick.exe convert ".\$favicon[0]" ".\favicon.png"
 }
