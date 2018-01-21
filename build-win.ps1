70b6fc10f59a4dd9675be9cb1c680088bd24ad7f
Shane <shane@shanerice.me>
2018-01-20 16:32:05 -0800

    Testing

--- build-win.ps1
+++ build-win.ps1
@@ -4,10 +4,15 @@ Write-Host "app_name:      $Env:app_name"
 Write-Host "app_url:       $Env:app_url"
 Write-Host "favicon_url:   $Env:favicon_url"
 
-$favicon = $path.split('/')[-1]
+$favicon = $Env:favicon_url.split('/')[-1]
 
 Write-Host "favicon:       $favicon"
+Write-Host "icon ext:      $favicon.split('.')[-1]"
 
 nativefier -V
 python -V
-convert -version
+magick.exe -version
+
+if ("$favicon.split('.')[-1]" -ne "png") {
+    magick.exe convert "$favicon" "favicon.png"
+}
